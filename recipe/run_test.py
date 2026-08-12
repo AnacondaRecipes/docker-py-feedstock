3515a7b7b5e8ffaef1927b5bbf78c69cacd4218d
imykhailov <imykhailov@anaconda.com>
2026-08-12 14:27:27 +0300

    fix: pkg_resources removed in setuptools; use importlib.metadata

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -11,14 +11,14 @@ if which('docker'):
     except docker.errors.DockerException as e:
         print(f"WARNING :: docker.DockerClient.from_env(version='auto') :: no Docker daemon reachable ({e}) :: SKIPPED")
     try:
-        import pkg_resources
-        pkg_resources.require('docker')
-        print("INFO :: pkg_resources.require('docker') :: PASSED")
+        import importlib.metadata
+        importlib.metadata.version('docker')
+        print("INFO :: importlib.metadata.version('docker') :: PASSED")
     except:
-        print("INFO :: pkg_resources.require('docker') :: FAILED")
+        print("INFO :: importlib.metadata.version('docker') :: FAILED")
         exit(1)
 else:
     print("WARNING :: which('docker') failed")
     print("WARNING :: docker.DockerClient.from_env() and")
-    print("WARNING :: pkg_resources.require('docker') tests skipped")
+    print("WARNING :: importlib.metadata.version('docker') tests skipped")
 exit(0)
